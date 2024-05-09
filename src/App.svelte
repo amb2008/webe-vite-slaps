83f60a29aad7ee4b6ea861a4b7d95a3e6704aa59
c26ab1 <c26ab1@dalton.org>
2024-05-09 09:41:59 -0400

    first push

---
+++ src/App.svelte
@@ -0,0 +1,70 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+
+  const suits = ["spade", "club", "heart", "diamond"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "jack",
+    "queen",
+    "king",
+    "ace",
+  ];
+
+  let deck = [];
+
+  // Create the deck of Uno cards
+  suits.forEach((suit) => {
+    values.forEach((value) => {
+      deck.push({ suit, value });
+    });
+  });
+
+  function shuffleDeck() {
+    deck.sort(() => Math.random() - 0.5);
+  }
+
+  shuffleDeck();
+
+  var cards_per = Math.round(deck.length/2)
+
+  console.log(deck);
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+</main>
+
+<style>
+</style>
